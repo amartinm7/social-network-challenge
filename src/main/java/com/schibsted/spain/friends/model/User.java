158a3b10af495f79ae19492ca55961ab9b19f9da
antoniomm <antonio.martinmoreno@zooplus.com>
2018-12-26 20:52:07 +0100

    defining the model

---
+++ src/main/java/com/schibsted/spain/friends/model/User.java
@@ -0,0 +1,87 @@
+package com.schibsted.spain.friends.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private final String name;
+    private final String password;
+
+    private User(String name, String password){
+        this.name = name;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("User{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public static class Builder{
+
+        private String name;
+        private String password;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        /**
+         * Username must be unique, from 5 to 10 alphanumeric characters.
+         * Password from 8 to 12 alphanumeric characters.
+         */
+        private void validate(){
+            final List<String> errorMessages = new ArrayList<>();
+            if(name == null){
+                errorMessages.add("The 'name' parameter is null.");
+            }
+            if(name.length() < 5 || name.length() > 10){
+                errorMessages.add(String.format("The length of the'name' parameter has to be between 5 and 10: %s.",name.length()));
+            }
+            if (!name.matches("[a-zA-Z0-9]*")){
+                errorMessages.add(String.format("The'name' parameter contains invalid characters. Only alphanumeric are valid characters: %s.",name.length()));
+            }
+            if(password == null){
+                errorMessages.add("The 'password' parameter is null.");
+            }
+            if(password.length() < 8 || password.length() > 12){
+                errorMessages.add(String.format("The length of the'password' parameter has to be between 8 and 12: %s.",password.length()));
+            }
+            if (!password.matches("[a-zA-Z0-9]*")){
+                errorMessages.add(String.format("The'password' parameter contains invalid characters. Only alphanumeric are valid characters: %s.",password.length()));
+            }
+            if (errorMessages.size() > 0){
+                throw new IllegalStateException(String.join("|| ", errorMessages));
+            }
+        }
+
+        public User build(){
+            validate();
+            return new User(name,password);
+        }
+    }
+}
